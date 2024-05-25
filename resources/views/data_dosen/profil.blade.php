2f5a996cfcfbb0abe769b9e54696c4e6c39cd1b8
ninanoptrina19 <203200113@almaata.ac.id>
2024-05-26 02:49:12 +0700

    tampilan password

--- resources/views/data_dosen/profil.blade.php
+++ resources/views/data_dosen/profil.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.dashboard')
 
 @section('content')
-<div class="container">
+<div class="container mt-4">
     <div class="row">
         <div class="col-md-8">
             <h2>Profil Dosen</h2>
@@ -14,6 +14,30 @@
                     <p><strong>Email:</strong> {{ $dosen->user->email }}</p>
                     <p><strong>Alamat:</strong> {{ $dosen->alamat }}</p>
                     <!-- Tambahkan field lain yang diperlukan -->
+
+                    <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Reset Password</button>
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                          <div class="modal-content">
+                            <div class="modal-header">
+                              <h1 class="modal-title fs-5" id="exampleModalLabel">Reset Password</h1>
+                              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                              <p class="text-danger">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, animi!</p>
+                              <form action="">
+                                <input type="password" class="form-control p-2 mb-3" placeholder="Password Lama">
+                                <input type="password" class="form-control p-2 mb-3" placeholder="Password Baru">
+                               
+                                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                    <input type="submit" class="btn btn-primary" value="Simpan Password">
+                                  
+                              </form>
+                            </div>
+                            
+                          </div>
+                        </div>
+                      </div>
                 </div>
             </div>
         </div>
